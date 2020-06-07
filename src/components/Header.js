4fb7f8c4f80b102a011c40c240616114927417f5
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-07 20:01:30 -0300

    Initial Animation State

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
@@ -18,9 +19,13 @@ const Header = () => {
             <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
           </svg>
         </div>
-        <div className="title">
+        <motion.div
+          className="title"
+          initial={{ y: -250 }}
+          animate={{ y: -10 }}
+        >
           <h1>Pizza Joint</h1>
-        </div>
+        </motion.div>
       </header>
     </Link>
   );
